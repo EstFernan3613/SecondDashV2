369ca09777b4672906f1122fba602b5664dc2296
juanmendez7 <110619700+juanmendez7@users.noreply.github.com>
2024-05-16 14:56:51 -0500

    textox tutro d
    xd

---
+++ Assets/Scripts/HUD/AvisoEscondite.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AvisoEscondite : MonoBehaviour
+{
+    public GameObject texto;
+    public bool Adentro1;
+
+
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            texto.SetActive(true);
+            Adentro1 = true;
+            
+            
+            if(Adentro1 == true && Input.GetKeyDown(KeyCode.Z))
+            {
+            texto.SetActive(false);
+            }
+
+        }
+    }
+
+}
