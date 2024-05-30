abaaebe6a9e1e2eec8cb6cd2585bb9f8780c47bc
juanmendez7 <110619700+juanmendez7@users.noreply.github.com>
2024-05-29 23:06:35 -0500

    enemigo policia nivel 2
    nivel 2

--- Assets/Policia/Scripts/Enemigo2D.cs
+++ Assets/Policia/Scripts/Enemigo2D.cs
@@ -4,25 +4,49 @@ using UnityEngine;
 
 public class Enemigo2D : MonoBehaviour
 {
-    public int rutina;
-    public float cronometro;
-    public Animator ani;
-    public int direccion;
-    public float speed_walk;
-    public float speed_run;
-    public GameObject target;
-    public bool atacando;
+  
+     public float speed = 5f; // Velocidad de movimiento del enemigo
+    public Transform player; // Referencia al transform del jugador
+    private Animator animator;
+    private bool isChasing = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        ani = GetComponent<Animator>();
-        target = GameObject.Find("PersonajeOfi");
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            isChasing = true;
+            animator.SetBool("estaCazando", isChasing);
+        }
     }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isChasing = false;
+            animator.SetBool("estaCazando", isChasing);
+        }
+    }
+
+    private void Update()
+    {
+        if (isChasing)
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+
+            // Ajusta la rotación del enemigo para que mire hacia el jugador
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            // Mueve al enemigo hacia el jugador
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+
+
 }
