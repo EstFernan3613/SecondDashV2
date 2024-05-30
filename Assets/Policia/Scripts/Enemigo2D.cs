05bd858e77f3526a41307b7ebedaa9b21d32c3ea
Juan Esteban Fernandez Cuervo <juan_est.fernandez@uao.edu.co>
2024-05-29 21:44:51 -0500

    CAMBIOS LEVEL 2

--- Assets/Policia/Scripts/Enemigo2D.cs
+++ Assets/Policia/Scripts/Enemigo2D.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Enemigo2D : MonoBehaviour
 {
+    public int rutina;
+    public float cronometro;
+    public Animator ani;
+    public int direccion;
+    public float speed_walk;
+    public float speed_run;
+    public GameObject target;
+    public bool atacando;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ani = GetComponent<Animator>();
+        target = GameObject.Find("PersonajeOfi");
     }
 
     // Update is called once per frame
